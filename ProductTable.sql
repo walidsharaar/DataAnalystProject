fb872e4585b2ebf368b86abde30f554e1cce3f19
Sharaar <walidsharar@gmail.com>
2021-07-24 14:36:22 +0200

    update on product table

--- ProductTable.sql
+++ ProductTable.sql
@@ -9,10 +9,13 @@ p.[Size] as [Product Size] ,
 p.[ProductLine] as [Product Line] ,
 p.[ModelName] as [Product Model Name] ,
 p.[EnglishDescription] as [Product Description] , 
+ps.EnglishProductSubcategoryName as SubCategory,
+pc.EnglishProductCategoryName as ProductCategory,
+
 ISNULL(p.[Status] , 'Outdated') as [Product Status]
 FROM
 [AdventureWorksDW2019].[dbo].[DimProduct] as p 
-left join dbo.DimProductSubcategory as ps on ps.productsubcategorykey = p.productSubcategoryKey 
-left join dbo.DimProductCategory as pc on ps.productcategorykey = pc.ProductCategoryKey 
+left join [AdventureWorksDW2019].dbo.DimProductSubcategory as ps on ps.productsubcategorykey = p.productSubcategoryKey 
+left join [AdventureWorksDW2019].dbo.DimProductCategory as pc on ps.productcategorykey = pc.ProductCategoryKey 
 order by 
 p.ProductKey asc
